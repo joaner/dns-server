00303d6209ea43b6701083ae7199f3019e956504
xiaokang pu <pxk@seiue.com>
2018-05-13 01:12:53 +0800

    parse question domain

--- src/index.js
+++ src/index.js
@@ -8,6 +8,22 @@ server.on('error', (err) => {
 
 server.on('message', (buf, rinfo) => {
   console.log(`server got: ${buf.toString('hex')} from ${rinfo.address}:${rinfo.port}`)
+
+  const question = buf.slice(12);
+
+  const domain = [];
+
+  let start = 0
+  let length
+  while (length = question.readUInt8(start)) {
+    const end = start + 1 + length
+    const part = question.slice(start, end)
+    domain.push(part.toString('utf8'))
+
+    start = end
+  }
+
+  console.log(`query ${domain.join('.')}`)
 })
 
 server.on('listening', () => {
