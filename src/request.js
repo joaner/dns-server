33ab46e401c9163d9e59c7136d1ba72ca77f26c6
xiaokang pu <pxk@seiue.com>
2018-05-15 00:38:43 +0800

    fix info code parse

--- src/request.js
+++ src/request.js
@@ -13,9 +13,18 @@ module.exports = class Request {
   getHeader() {
     const section = this.buffer.slice(0, 12)
 
+    const info = section.readUInt16BE(2)
+
     const header = {
       ID: section.readUInt16BE(0),
-      INFO: section.readUInt16BE(2),
+      QR: info >>> 31, // 1 bit
+      OPCODE: info << 1 >>> 28, // 4 bits
+      AA: info << 5 >>> 31, // 1 bit
+      TC: info << 6 >>> 31, // 1 bit
+      RD: info << 7 >>> 31, // 1 bit
+      RA: info << 8 >>> 31, // 1 bit
+      Z: info << 9 >>> 29, // 3 bits
+      RCODE: info << 12 >>> 28, // 4 bits
       QDCOUNT: section.readUInt16BE(4),
       ANCOUNT: section.readUInt16BE(6),
       NSCOUNT: section.readUInt16BE(8),
